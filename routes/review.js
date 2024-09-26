1c707a28724f1bfe858021522ba3aa28c8c550a6
Dilshad Alam <da3802625@gmail.com>
2024-09-26 17:53:47 +0530

    Add Project Files

---
+++ routes/review.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+const { validateReview, isLoggedIn, isreviewAuthor } = require("../middleware.js");
+
+const reviewController = require("../controllers/reviews.js");
+const review = require("../models/review.js"); 
+
+//reviews/Post route
+router.post("/",
+    isLoggedIn,
+    validateReview,
+    wrapAsync(reviewController.createReview)
+);
+
+//reviews route/Delete
+router.delete("/:reviewId",
+    isLoggedIn,
+    isreviewAuthor,
+    wrapAsync(reviewController.destroyReview)
+);
+
+module.exports = router;
